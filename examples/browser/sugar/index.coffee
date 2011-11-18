3240329bf7beec39aad895e9a97a92a17bbd7f37
Mike Nichols <nichols.mike.s@gmail.com>
2011-11-18 16:20:41 -0700

    added support for namespacing templates according to directory structure

--- examples/browser/sugar/index.coffee
+++ examples/browser/sugar/index.coffee
@@ -1,3 +1,6 @@
 $().ready ->
   $('body').append templates.template1(stooges: ['moe', 'larry', 'curly'])
   $('body').append templates.template2({ album : 'Joes Garage'})
+  $('#nested').append templates.nested.a()
+  $('#nested').append templates.nested.deep.c()
+  $('#nested').append templates.nested2.a()
--- examples/browser/sugar/index.js
+++ examples/browser/sugar/index.js
@@ -3,8 +3,11 @@
     $('body').append(templates.template1({
       stooges: ['moe', 'larry', 'curly']
     }));
-    return $('body').append(templates.template2({
+    $('body').append(templates.template2({
       album: 'Joes Garage'
     }));
+    $('#nested').append(templates.nested.a());
+    $('#nested').append(templates.nested.deep.c());
+    return $('#nested').append(templates.nested2.a());
   });
 }).call(this);
